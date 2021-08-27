6045e6732f77db6ce60d4e2ce095f67a2bef125f
Eric Devine <80430372+BigPorg@users.noreply.github.com>
2021-08-26 21:29:39 -0400

    added to typeDefs, Date

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,6 @@
 // match up with events and users in schemas
 // https://www.apollographql.com/docs/apollo-server/schema/custom-scalars/
+// https://hasura.io/blog/working-with-dates-time-timezones-graphql-postgresql/
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -7,21 +8,18 @@ const typeDefs = gql`
         _id: ID
         name: String
     }
-
     type Event {
         _id: ID
         name: String
         count: Int
-        date: TODO
-        time: TODO
+        date: Date
+        time: Float
         description: String
     }
-
     type Auth {
         token: ID
         user: User
     }
-
     type Query {
         findOldEvent: [OldEvent]
         findCurrentEvent: [CurrentEvent] 
@@ -30,13 +28,24 @@ const typeDefs = gql`
         TODO work on this
         checkout TODO with Stripe
     }
-
     type Mutation {
         addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
         updateUser(firstName: String, lastName: String, email: String, password: String): User
         login(email: String!, password: String!): Auth
-        addEvent([TODO])
-        modifyEvent([TODO])
-        deleteEvent([TODO])
+        addEvent(name: String!, date: Date, time: Float, description: String)
+        modifyEvent(name: String!)
+        deleteEvent(name: String!)
     }
+    type Checkout {
+        session: ID
+      }
+    type Order {
+        _id: ID
+        purchaseDate: String
+        products: [Product]
+      }
 `;
+
+module.exports = typeDefs;
+
+// added temp checkout and order for stripe stuff

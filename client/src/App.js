54b0662031e9e4d94e5184f27a2395b097801f5b
Matthew Shafransky <matt@Matthews-MacBook-Pro.local>
2021-08-28 13:13:00 -0400

    dashboard and footer added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header/header.js';
 import Body from './components/body.js'
+import Footer from './components/footer.js'
 import '../src/App.css'
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   )
 }
--- client/src/components/body.js
+++ client/src/components/body.js
@@ -2,8 +2,16 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
-//import itemData from './itemData';
+import Typography from '@material-ui/core/Typography';
 import image from '../photos/adi-goldstein-Hli3R6LKibo-unsplash.jpg';
+import image2 from '../photos/christina-wocintechchat-com-Q80LYxv_Tbs-unsplash.jpg';
+import image3 from '../photos/jeremy-wong-weddings-464ps_nOflw-unsplash.jpg';
+import image4 from '../photos/photos-by-lanty-O38Id_cyV4M-unsplash.jpg';
+import image5 from '../photos/redd-PTRzqc_h1r4-unsplash.jpg';
+import Box from '@material-ui/core/Box';
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +28,11 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 
+
+      
+      
+
+
   //The example data is structured as follows:
  
   //import image from 'path/to/image.jpg';
@@ -30,29 +43,65 @@ const useStyles = makeStyles((theme) => ({
       img: image,
       title: 'example',
       author: 'author',
-      cols: 2,
+      cols: 3,
     },
     {
-        img: image,
+        img: image2,
         title: 'example',
         author: 'author',
-        cols: 2,
+        cols: 1,
+      },
+      {
+        img: image3,
+        title: 'example',
+        author: 'author',
+        cols: 1,
+      },
+      {
+        img: image4,
+        title: 'example',
+        author: 'author',
+        cols: 3,
+      },
+      {
+        img: image5,
+        title: 'example',
+        author: 'author',
+        cols: 4,
       },
 
   ];
  
-export default function BasicImageList() {
+export default function Body() {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <ImageList rowHeight={160} className={classes.imageList} cols={3}>
+      <Box width="25%">
+      <Typography variant="body1" align="left" color="primary" display="flex">
+      Eventful is a User-Friendly event planning application designed to take control of your event.
+      This will allow you to cut out the middle-man and take matters into your own hands. 
+      Eventful is perfect for Weddings, Meetings, Birthdays, and much more!
+      
+      </Typography>
+      </Box>
+      <ImageList rowHeight={160} className={classes.imageList} cols={4}>
         {itemData.map((item) => (
           <ImageListItem key={item.img} cols={item.cols || 1}>
             <img src={item.img} alt={item.title} />
           </ImageListItem>
         ))}
       </ImageList>
+      <Box width="25%">
+      <Typography variant="body1" align="left" color="primary" display="flex">
+      Start your experience with Eventful today! Just click the "Log In" button above!
+      </Typography>
+      </Box>
     </div>
   );
 }
+
+
+
+
+

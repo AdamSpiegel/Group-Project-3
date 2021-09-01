9c86a4f32ff48e9b95e722f68077c45b535e88db
Your Name <you@example.com>
2021-08-31 22:08:50 -0400

    App.js updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/header/header.js';
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from './components/footer.js'
 import '../src/App.css'
 import Dashboard from './pages/Dashboard.js'
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,9 @@
-
-import React from "react";
-import {useHistory} from "react-router-dom";
-import Header from '../components/header/header.js';
-import Footer from '../components/footer.js'
-
-
-
-
-
-
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useMutation } from '@apollo/client';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
+
 
 function Login(props) {
     const [formState, setFormState] = useState({ email: '', password: '' });
@@ -43,7 +32,7 @@ function Login(props) {
 
     return (
         <div className="container my-1">
-            {/* <Link to="/signup">← Go to Signup</Link> */}
+            <Link to="/signup">← Go to Signup</Link>
 
             <h2>Login</h2>
             <form onSubmit={handleFormSubmit}>
